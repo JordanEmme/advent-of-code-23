9540de5aac0f2374cc92118260bfd1f64f408968
Jordan Emme <jordanemme@protonmail.com>
2023-12-02 22:15:10 +0000

    day one part two

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -7,7 +7,27 @@ pub fn solution_part_one(input: &str) -> u16 {
 }
 
 pub fn solution_part_two(input: &str) -> u16 {
-    0
+    let mut sum: u16 = 0u16;
+    input.lines().for_each(|line| {
+        let rewritten_line = rewrite(line);
+        sum += find_line_number(&*rewritten_line);
+    });
+    return sum;
+}
+
+fn rewrite(original_line: &str) -> String {
+    let mut rewritten_line: String;
+    rewritten_line = original_line.replace("zero", "0");
+    rewritten_line = rewritten_line.replace("one", "o1e"); // because of twone and oneight
+    rewritten_line = rewritten_line.replace("two", "t2"); //because of eightwo
+    rewritten_line = rewritten_line.replace("three", "t3e"); // because of eighthree and threeight
+    rewritten_line = rewritten_line.replace("four", "4");
+    rewritten_line = rewritten_line.replace("five", "5e"); // because of fiveeight
+    rewritten_line = rewritten_line.replace("six", "6");
+    rewritten_line = rewritten_line.replace("seven", "7n"); // because of sevenine
+    rewritten_line = rewritten_line.replace("eight", "e8"); // because of nineight
+    rewritten_line = rewritten_line.replace("nine", "9");
+    return rewritten_line;
 }
 
 fn find_line_number(line: &str) -> u16 {
